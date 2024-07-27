b3628bf96923e893895006d81a00cf2688819c1b
jarvis <ivan.sergeev.gorbachov@gmail.com>
2024-07-27 17:26:07 +0200

    All structured and no err, but, also, no output

--- Vigenere cipher/Functions/num_to_letter.mjs
+++ Vigenere cipher/Functions/num_to_letter.mjs
@@ -1,5 +1,12 @@
 const numToLetter = function (input, alphabet) {
-    
+    const output = [];
+    output.length = input.length;
+
+    output.forEach((el, indx) => {
+        output[indx] = alphabet[input[indx]];
+    })
+
+    return output.join('');
 }
 
 export default numToLetter;
--- Vigenere cipher/vigenere_cipher.mjs
+++ Vigenere cipher/vigenere_cipher.mjs
@@ -1,10 +1,19 @@
 import {
-    exportJsAlphabet as strAlphabet, 
-    currentTxtAlphabet as alphabet
-} from '../universal_alphabet/txt_to_js_transformation.mjs'
+    currentTxtAlphabet as strAlphabet, 
+    exportJsAlphabet as alphabet
+} from '../universal_alphabet/txt_to_js_transformation.mjs';
 
-const vigenereCipher = function (input, encryptOrDecrypt, key, output = []) {
+import sumByModule from './Functions/sum_by_module.mjs';
+import numToLetter from './Functions/num_to_letter.mjs';
 
+const vigenereCipher = function (input, encryptOrDecrypt, key, alphabet) {
+    //Make arr with sum by module alphabet.length
+    const sumedByModuleArr = sumByModule(input, key, encryptOrDecrypt, alphabet);
+
+    //Translate arr to letter code
+    const numToLetterArr = numToLetter(sumedByModuleArr, alphabet);
+
+    return numToLetterArr;
 }
 
-const output = vigenereCipher()
+const output = vigenereCipher('Hello World', true, 'key', alphabet);
