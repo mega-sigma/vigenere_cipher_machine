1e81facd9da2b553467fbe518005638e92a7b474
jarvis <ivan.sergeev.gorbachov@gmail.com>
2024-08-05 16:59:25 +0200

    London problem solved - changed Universal alphabet. Simbol '-' now apears only on index 41

--- Universal alphabet/txt_to_js_transformation.mjs
+++ Universal alphabet/txt_to_js_transformation.mjs
@@ -1,4 +1,4 @@
-const currentTxtAlphabet = `a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 ! ? . , _ - [ ] { } ( ) @ # $ % ^ & * ~ / + - < > : ; " ' |` + ' `'; //copied from the alphabet.txt
+const currentTxtAlphabet = `a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 ! ? . , _ - [ ] { } ( ) @ # $ % ^ & * ~ / + < > : ; " ' |` + ' `'; //copied from the alphabet.txt
 const exportJsAlphabet = currentTxtAlphabet.split(' ');
 
 exportJsAlphabet.push(' '); //and add space simbol
